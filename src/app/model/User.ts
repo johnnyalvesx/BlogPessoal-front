478c80cfe124d01f9ec08bc3e3fd9b68ecaf4fad
johnnyalvesx <alvesjohnny@outlook.com>
2021-11-04 19:21:47 -0300

    Adição do campo data de nascimento

--- src/app/model/User.ts
+++ src/app/model/User.ts
@@ -5,6 +5,7 @@ export class User {
     public nome: string
     public usuario: string
     public senha: string
+    public dataNascimento: string
     public foto: string
     public tipo: string
     public postagem: Postagem[]
